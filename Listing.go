052119730ad9016f89b112f49e2023f2e60caca8
Kabilan <piccotvm73@gmail.com>
2025-06-19 17:41:58 +0530

    List functions

--- Listing.go
+++ Listing.go
@@ -2,6 +2,8 @@ package listing
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/spurtcms/auth/migration"
 )
@@ -20,4 +22,129 @@ func ListingSetup(config Config) *Listing {
 		DB:               config.DB,
 	}
 
+}
+
+func (listing *Listing) ListingsList(limit, offset int, filter Filter, tenantid string) (list []TblListing, Count int64, err error) {
+
+	if Autherr := AuthandPermission(listing); Autherr != nil {
+
+		return []TblListing{}, 0, Autherr
+	}
+
+	if filter.ContentType == "Course" {
+
+		filter.ContentType = strings.ToLower(filter.ContentType)
+
+	} else if filter.ContentType == "Channel" {
+
+		filter.ContentType = strings.ToLower(filter.ContentType)
+
+	}
+
+	if filter.PaymentType == "Price" {
+
+		filter.PaymentType = strings.ToLower(filter.PaymentType)
+
+	} else if filter.PaymentType == "Membership" {
+
+		filter.PaymentType = strings.ToLower(filter.PaymentType)
+
+	}
+
+	listinglist, _, _ := Listingmodels.ListingList(limit, offset, filter, tenantid, listing.DB)
+
+	_, count, err := Listingmodels.ListingList(0, 0, filter, tenantid, listing.DB)
+	if err != nil {
+
+		return []TblListing{}, 0, err
+	}
+
+	return listinglist, count, nil
+
+}
+
+func (listing *Listing) CreateListing(create TblListing) error {
+
+	if Autherr := AuthandPermission(listing); Autherr != nil {
+
+		return Autherr
+	}
+
+	createdon, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	Create := TblListing{
+		Title:        create.Title,
+		Description:  create.Description,
+		ContentType:  create.ContentType,
+		ContentId:    create.ContentId,
+		EntryId:      create.EntryId,
+		IsDeleted:    0,
+		IsActive:     1,
+		CreatedOn:    createdon,
+		CreatedBy:    create.CreatedBy,
+		ImagePath:    create.ImagePath,
+		ImageName:    create.ImageName,
+		PaymentType:  create.PaymentType,
+		Price:        create.Price,
+		MembershipId: create.MembershipId,
+		TenantId:     create.TenantId,
+	}
+
+	err := Listingmodels.CreateListing(Create, listing.DB)
+
+	if err != nil {
+
+		return err
+	}
+
+	return nil
+
+}
+
+func (listing *Listing) DeleteListing(id, userid int, tenantid string) error {
+
+	if Autherr := AuthandPermission(listing); Autherr != nil {
+
+		return Autherr
+	}
+
+	deletedon, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	deletedby := userid
+
+	err := Listingmodels.DeleteListing(id, tenantid, deletedby, deletedon, listing.DB)
+
+	if err != nil {
+
+		return err
+	}
+
+	return nil
+
+}
+
+func (listing *Listing) MultiSelectDeleteListing(listingids []int, modifiedby int, tenantid string) error {
+
+	if Autherr := AuthandPermission(listing); Autherr != nil {
+
+		return Autherr
+	}
+
+	var Listing TblListing
+
+	Listing.DeletedBy = modifiedby
+
+	Listing.DeletedOn, _ = time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	Listing.IsDeleted = 1
+
+	Listing.TenantId = tenantid
+
+	err := Listingmodels.MultiSelectListingsDelete(&Listing, listingids, listing.DB)
+	if err != nil {
+
+		return err
+
+	}
+	return nil
 }
--- model.go
+++ model.go
@@ -1,7 +1,125 @@
 package listing
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 func helo() {
 	fmt.Println("Heloo")
+}
+
+type Filter struct {
+	Keyword     string
+	Title       string
+	ContentType string
+	PaymentType string
+}
+
+type TblListing struct {
+	Id              int       `gorm:"primaryKey;auto_increment;type:serial"`
+	Title           string    `gorm:"type:character varying"`
+	Description     string    `gorm:"type:character varying"`
+	ContentType     string    `gorm:"type:character varying"`
+	ContentId       int       `gorm:"type:integer"`
+	EntryId         int       `gorm:"type:integer"`
+	IsDeleted       int       `gorm:"type:integer"`
+	DeletedOn       time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
+	DeletedBy       int       `gorm:"DEFAULT:NULL"`
+	IsActive        int       `gorm:"type:integer"`
+	CreatedOn       time.Time `gorm:"type:timestamp without time zone"`
+	CreatedBy       int       `gorm:"type:integer"`
+	ImageName       string    `gorm:"type:character varying"`
+	ImagePath       string    `gorm:"type:character varying"`
+	PaymentType     string    `gorm:"type:character varying"`
+	Price           int       `gorm:"type:integer"`
+	MembershipId    int       `gorm:"type:integer"`
+	TenantId        string    `gorm:"type:character varying"`
+	MembershipLevel string    `gorm:"-"`
+}
+
+type ListingModel struct {
+	Userid     int
+	DataAccess int
+}
+
+var Listingmodels ListingModel
+
+func (Listingmodel ListingModel) ListingList(limit, offset int, filter Filter, tenantid string, DB *gorm.DB) (listing []TblListing, count int64, err error) {
+
+	query := DB.Table("tbl_listings").Where("tbl_listings.is_deleted=0 and tbl_listings.tenant_id=?", tenantid).Order("created_on desc")
+
+	if filter.Keyword != "" {
+
+		query = query.Where("LOWER(TRIM(tbl_listings.title)) like LOWER(TRIM(?))", "%"+filter.Keyword+"%")
+
+	}
+
+	if filter.Title != "" {
+
+		query = query.Where("LOWER(TRIM(tbl_listings.title)) like LOWER(TRIM(?))", "%"+filter.Title+"%")
+
+	}
+
+	if filter.ContentType != "" {
+
+		query = query.Where("tbl_listings.content_type=?", filter.ContentType)
+
+	}
+
+	if filter.PaymentType != "" {
+
+		query = query.Where("tbl_listings.payment_type=?", filter.PaymentType)
+
+	}
+
+	if limit != 0 {
+
+		query.Limit(limit).Offset(offset).Find(&listing)
+
+		return listing, count, nil
+
+	}
+
+	query.Find(&listing).Count(&count)
+	if query.Error != nil {
+
+		return []TblListing{}, 0, query.Error
+	}
+
+	return listing, count, nil
+}
+
+func (Listingmodel ListingModel) CreateListing(listing TblListing, DB *gorm.DB) error {
+
+	if err := DB.Table("tbl_listings").Create(&listing).Error; err != nil {
+
+		return err
+	}
+
+	return nil
+
+}
+
+func (Listingmodel ListingModel) DeleteListing(id int, tenantid string, deletedby int, deletedon time.Time, DB *gorm.DB) error {
+
+	if err := DB.Table("tbl_listings").Where("id=? and tenant_id=?", id, tenantid).UpdateColumns(map[string]interface{}{"is_deleted": 1, "deleted_by": deletedby, "deleted_on": deletedon}).Error; err != nil {
+
+		return err
+	}
+
+	return nil
+}
+
+func (Listingmodel ListingModel) MultiSelectListingsDelete(listing *TblListing, id []int, DB *gorm.DB) error {
+
+	if err := DB.Table("tbl_listings").Where("id in (?) and tenant_id=?", id, listing.TenantId).UpdateColumns(map[string]interface{}{"is_deleted": listing.IsDeleted, "deleted_on": listing.DeletedOn, "deleted_by": listing.DeletedBy}).Error; err != nil {
+
+		return err
+	}
+
+	return nil
+
 }
